e6f7cea58ec1a4276210a1f98e7acd7704d924b0
teo-o <mateoorozco313199@correo.itm.edu.co>
2022-11-09 15:32:36 -0500

    Create a repository

---
+++ src/Desafio_1/Persona.java
@@ -0,0 +1,73 @@
+package Desafio_1;
+
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private String dni;
+    private String direccion;
+    private String telefono;
+    private String email;
+
+    public Persona(String nombre, String apellido, String dni, String direccion, String telefono, String email) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "nombre=" +nombre + ", apellido=" + apellido + ", dni=" + dni + ", direccion=" + direccion + ", telefono=" + telefono + ", email=" + email;
+    }
+
+}
